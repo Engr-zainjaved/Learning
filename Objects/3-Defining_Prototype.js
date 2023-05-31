6e45f98c330eadc778b808a82710c412e7d7dcc6
Engr-zainjaved <2017ch272@student.uet.edu.pk>
2023-05-31 18:44:25 +0500

    summary in defining prototype added

--- Objects/3-Defining_Prototype.js
+++ Objects/3-Defining_Prototype.js
@@ -24,3 +24,14 @@ newCourse.id = "My new course id";
 console.log(newCourse.id)       // this will return newCourse id as it is present in it...even if the same id is also present in it's prototype
 
 // JS only seek method in prototype or prototype's prototype if the id is not present in the newly created object.
+
+//____________________________________________________
+
+            // SUMMARY
+
+// Many objects are predefined
+// Objects link to prototypes which define their behaviour
+// Calling a property on an object will look through the prototype chain
+// Object literlas link to Object.prototype
+// Object.prototype sits at the end of prototype chains
+// An object's prototype can be set....(1) With the "new" syntax...(2)by using object.create()
