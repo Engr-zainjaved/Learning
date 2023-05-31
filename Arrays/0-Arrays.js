bfe4cc3a4a812aa90cfb63e92ff9aef6291610bd
Engr-zainjaved <2017ch272@student.uet.edu.pk>
2023-05-31 23:16:23 +0500

    updated the 0-Arrays.js

--- Arrays/0-Arrays.js
+++ Arrays/0-Arrays.js
@@ -1,1 +1,12 @@
-//? ARRAYS IN JAVASCRIPT
+// ARRAYS IN JAVASCRIPT
+
+// ARRAYS ARE LIKE OBJECTS
+// arrays contains properties
+// access properties with bracket notation
+// array functions come from their prototype
+// can create arrays in many of the same ways as objects
+
+// WAYS ARRAYS ARE UNLIKE OBJECTS
+// properties are zero-based numbers
+// cannot use dot notation(.) to access its properties
+// new Array.prototype functions which are specific to arrays only
